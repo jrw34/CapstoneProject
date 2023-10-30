98ee353e4e6e0ce5b8f32b9471b4f477ad4c8470
jrw34 <johnrwalsh34@gmail.com>
2023-10-29 19:08:12 -0500

    Changed import step to just import pre-parsed csv file

--- CapstoneProject.py
+++ CapstoneProject.py
@@ -11,22 +11,8 @@ from display_perfect_match import*
 
 
 
-#read data from 'branded_food.csv'
-data = pd.read_csv("C:\\Users\\16185\\Desktop\\branded apr 2023\\branded_food.csv", low_memory=False)
-
-#read data from 'food.csv' to merge with data to include 'food_category_id'
-category_data = pd.read_csv("C:\\Users\\16185\\Desktop\\branded apr 2023\\food.csv")
-
-#merge columns to that category_id is included in the dataframe for analysis/modelling
-df = data.merge(right = category_data, on = 'fdc_id')
-
-
-#create ingred_list column containing sets of parsed ingredients
-df['ingred_list'] = df.ingredients.apply(lambda x: ingredient_parser(x, comma_tags, empty_tags))
-
-#drop all rows where 'description' is nan
-df_dropped = df.dropna(axis = 0, subset = 'description')
-
+#read in dataframe from parsed_branded.csv
+df = pd.read_csv("C:\Users\16185\Desktop\parsed_branded_food.csv")
 
 #test Orange Juice Query and Display
 search_str = 'Orange Juice'
@@ -34,7 +20,7 @@ prioritize = ['Organic']
 avoid = ['Sugar', 'Sodium Benzoate', 'High Fructose Corn Syrup']
 features = ['Pulp Free']
 
-display_perfect_matches(df_dropped, search_str, prioritize, avoid, features)
+display_perfect_matches(df, search_str, prioritize, avoid, features)
 
 
 
