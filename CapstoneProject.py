8501f30851de4166024dbdbf56662b57df6f5644
jrw34 <johnrwalsh34@gmail.com>
2023-10-29 19:13:29 -0500

    Updated read_csv to include low_memory=False

--- CapstoneProject.py
+++ CapstoneProject.py
@@ -1,7 +1,6 @@
 import pandas as pd 
 import numpy as np
 import re
-import networkx as nx
 import matplotlib.pyplot as plt
 import plotly.graph_objs as go
 from collections import Counter
@@ -12,7 +11,7 @@ from display_perfect_match import*
 
 
 #read in dataframe from parsed_branded.csv
-df = pd.read_csv("C:\\Users\\16185\\Desktop\\parsed_branded_food.csv")
+df = pd.read_csv("C:\\Users\\16185\\Desktop\\parsed_branded_food.csv", low_memory=False)
 
 #test Orange Juice Query and Display
 search_str = 'Orange Juice'
