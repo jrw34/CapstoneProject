3ed1a7275efd3cf85c5207758f8c6053f38977fd
jrw34 <johnrwalsh34@gmail.com>
2023-10-30 22:18:17 -0500

    Cannot establish connection but the file path is now correct

--- push_data_to_db.py
+++ push_data_to_db.py
@@ -2,12 +2,16 @@ import pickle
 import os
 from sqlalchemy import create_engine
 
+#extract DB_URL
 DB_URL = os.environ['DB_URL']
+#create engine to connect to postgres DB
+engine = create_engine(DB_URL)
 
+#create filepath to load pkl file 
 filepath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\\parsed_branded.pkl"
 
 with open(filepath, 'rb') as f:
     parsed_df = pickle.load(f)
     
-engine = create_engine(DB_URL)
+#create new table in DB consisting of parsed branded food items dataset
 parsed_df.to_sql("PARSED BRANDED FOODS", engine)
