1744b69fbfb0b1fb13a1ebfb0e12037970337d0a
jrw34 <johnrwalsh34@gmail.com>
2023-10-30 22:00:12 -0500

    Updated Environmental Variables in create_engine call

--- push_data_to_db.py
+++ push_data_to_db.py
@@ -2,7 +2,7 @@ import pickle
 import os
 from sqlalchemy import create_engine
 
-DB_URL = os.getenv('DB_URL')
+DB_URL = os.environ['DB_URL']
 
 filepath = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop') + "\\parsed_branded.pkl"
 
