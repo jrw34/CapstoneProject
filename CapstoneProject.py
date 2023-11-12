765555bdb5254c23f4d86e001ca37cf3adad9f97
jrw34 <johnrwalsh34@gmail.com>
2023-11-11 19:50:44 -0600

    Added graphical display to main project pipeline

--- CapstoneProject.py
+++ CapstoneProject.py
@@ -10,7 +10,7 @@ import os
 
 #import user-defined functions 
 from ingredient_parser import*
-from ingredient_query import*
+from ingredient_request import*
 from display_perfect_match import*
 from description_query import*
 
@@ -18,27 +18,26 @@ from description_query import*
 search_str = 'Orange Juice'.upper()
 
 #query DB for all items containing search_str
-query_df = ingredient_query(search_str)
+query_df = description_query_db(search_str)
 
 #test Orange Juice Query and Display
 
-prioritize = ['Organic']
-avoid = ['Sugar', 'Sodium Benzoate', 'High Fructose Corn Syrup']
-features = ['No Pulp']
+prioritize = ['Organic'.upper()]
+avoid = ['Sodium Benzoate'.upper(), 'HIGH FRUCTOSE CORN SYRUP']
+features = ['No Pulp'.upper()]
 
 #assess counts from lists above for their presence in query_df  
 assessed_query = assess_query(query_df, prioritize, avoid, features)
 
 #filter assessed_query so that it only contains perfect matches
-perfect_matches = perfect_match(assessed_query, prioritize, avoid, features)
+perfect_matches = perfect_match_from_assessed(assessed_query, prioritize, avoid, features)
 
 #convert perfect matches to graph dict
-graph_dict = ...
+graph_dict = perfect_match_to_graph_dict(perfect_matches, prioritize, avoid, features)
 
-#generate position dict from graph dict
-position_dict = ...
 
 #plot network graph 
+display_perfect_matches(perfect_matches, search_str, prioritize, avoid, features)
 
 
 
